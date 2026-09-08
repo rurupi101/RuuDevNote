aed2cde5c5502465d18646dc8b03e39778efa773
Hyeonjeong <77945724+258xsw@users.noreply.github.com>
2026-09-08 16:43:02 +0900

    Configure GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://rurupi101.github.io/RuuDevNote/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
