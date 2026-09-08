8552f0ff514c25716457fcb4122105a67c50c6ba
Hyeonjeong <77945724+258xsw@users.noreply.github.com>
2026-09-08 16:51:43 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://rurupi101.github.io/RuuDevNote/',
+	base: '/RuuDevNote',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
