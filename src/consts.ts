a508afb5b4a35bfad323359bf3ece436bf138565
Hyeonjeong <77945724+258xsw@users.noreply.github.com>
2026-09-08 17:48:13 +0900

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'RuuDevNote';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = '유니티 개발자가 될테야';
